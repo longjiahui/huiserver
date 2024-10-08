39efe52ac98ea42bc07729717f6f9dc2517e1886
Anfo <longjiahui@hotmail.com>
2024-10-08 14:28:30 +0800

    feat: v1.0.13

--- src/module/http/guard/validator.ts
+++ src/module/http/guard/validator.ts
@@ -1,66 +1,94 @@
-import { ValidateError } from "../../../error"
-import type { Context, Module } from "../../../framework/type"
-import type { Next } from "koa"
-import { Validator, type RuleArg } from "@anfo/validator"
-import { createGuard, createModule } from "../../../framework/util"
-import { pick } from "lodash"
+import { ValidateError } from '../../../error'
+import type { Context, Module } from '../../../framework/type'
+import type { Next } from 'koa'
+import { Validator, type RuleArg } from '@anfo/validator'
+import { createGuard, createModule } from '../../../framework/util'
 
 const validator = new Validator()
 export const v = createGuard(
-  (
-    rule: { body?: RuleArg; params?: RuleArg; query?: RuleArg } = {},
-    options: {
-      // true 只有rule中列出的参数可以通过
-      onlyKeysInRule?: boolean
-    } = {}
-  ) => {
-    const { onlyKeysInRule } = Object.assign(
-      {
-        onlyKeysInRule: true,
-      } satisfies typeof options,
-      options
-    )
-    return async (ctx: Context, next: Next) => {
-      const failed = (err: string) => {
-        throw new ValidateError(`params validate failed: ${err}`)
-      }
-      function getKeys(value: any) {
-        return Object.keys(value).map((k) =>
-          k.endsWith("$") ? k.slice(0, -1) : k
+    (
+        rule: { body?: RuleArg; params?: RuleArg; query?: RuleArg } = {},
+        options: {
+            // true 只有rule中列出的参数可以通过
+            onlyKeysInRule?: boolean
+        } = {}
+    ) => {
+        const { onlyKeysInRule } = Object.assign(
+            {
+                onlyKeysInRule: true,
+            } satisfies typeof options,
+            options
         )
-      }
-      if (rule.query) {
-        const err = await validator.v(ctx.query, rule.query)
-        if (err) {
-          failed(err)
+        return async (ctx: Context, next: Next) => {
+            const failed = (err: string) => {
+                throw new ValidateError(`params validate failed: ${err}`)
+            }
+            // function getKeys(value: any) {
+            //     return Object.keys(value).map((k) =>
+            //         k.endsWith('$') ? k.slice(0, -1) : k
+            //     )
+            // }
+            function _pick<
+                T extends Record<any, any>,
+                From extends Record<any, any>
+            >(d: T, from: From) {
+                return Object.keys(from).reduce((t, k) => {
+                    k = k.endsWith('$') ? k.slice(0, -1) : k
+                    t[k as keyof From] =
+                        from[k] && typeof from[k] === 'object'
+                            ? _pick(d[k], from[k])
+                            : d[k]
+                    return t
+                    // 这里的类型是不准确的，但不重要。
+                }, {} as { [k in keyof From]: T[k] })
+            }
+            if (rule.query) {
+                const err = await validator.v(ctx.query, rule.query)
+                if (err) {
+                    failed(err)
+                }
+                if (
+                    onlyKeysInRule &&
+                    typeof rule.query === 'object' &&
+                    !(rule.query instanceof RegExp) &&
+                    !(rule.query instanceof Array)
+                ) {
+                    ctx.query = _pick(ctx.query, rule.query)
+                }
+            }
+            if (rule.body) {
+                const err = await validator.v(ctx.request.body, rule.body)
+                if (err) {
+                    failed(err)
+                }
+                if (
+                    onlyKeysInRule &&
+                    typeof rule.body === 'object' &&
+                    !(rule.body instanceof RegExp) &&
+                    !(rule.body instanceof Array)
+                ) {
+                    ctx.request.body = _pick(ctx.request.body || {}, rule.body)
+                }
+            }
+            if (rule.params) {
+                const err = await validator.v(ctx.params, rule.params)
+                if (err) {
+                    failed(err)
+                }
+                if (
+                    onlyKeysInRule &&
+                    typeof rule.params === 'object' &&
+                    !(rule.params instanceof RegExp) &&
+                    !(rule.params instanceof Array)
+                ) {
+                    ctx.params = _pick(ctx.params, rule.params)
+                }
+            }
+            return await next()
         }
-        if (onlyKeysInRule) {
-          ctx.query = pick(ctx.query, getKeys(rule.query))
-        }
-      }
-      if (rule.body) {
-        const err = await validator.v(ctx.request.body, rule.body)
-        if (err) {
-          failed(err)
-        }
-        if (onlyKeysInRule) {
-          ctx.request.body = pick(ctx.request.body, getKeys(rule.body))
-        }
-      }
-      if (rule.params) {
-        const err = await validator.v(ctx.params, rule.params)
-        if (err) {
-          failed(err)
-        }
-        if (onlyKeysInRule) {
-          ctx.params = pick(ctx.params, getKeys(rule.params))
-        }
-      }
-      return await next()
     }
-  }
 )
 
 export default createModule((app) => {
-  app.guard.v = v
+    app.guard.v = v
 })
