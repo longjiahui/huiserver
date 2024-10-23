0813b917443195c317a6675753095095f1ac16cc
Anfo <longjiahui@hotmail.com>
2024-10-23 11:48:18 +0800

    fix: fix env bugs

--- src/env.ts
+++ src/env.ts
@@ -1,19 +1,19 @@
 import { defaultLogger } from './framework/logger'
 import dotenv from 'dotenv'
-import path from 'node:path'
-import fs from 'node:fs'
 
 const initEnv = () => {
     dotenv.config()
-    let dotenvFile = path.resolve(
-        '..',
-        `.env${process.env.NODE_ENV && `.${process.env.NODE_ENV}||''`}`
-    )
+    let extraDotEnvFile = `.env${
+        process.env.NODE_ENV && `.${process.env.NODE_ENV || ''}`
+    }`
     defaultLogger.log('process.NODE_ENV: ', process.env.NODE_ENV)
     defaultLogger.log('.env file is loaded')
-    if (fs.existsSync(dotenvFile)) {
-        dotenv.config({ path: dotenvFile, override: true })
-        defaultLogger.log(`${dotenvFile} file is loaded`)
+    if (extraDotEnvFile) {
+        dotenv.config({
+            path: extraDotEnvFile,
+            override: true,
+        })
+        defaultLogger.log(`${extraDotEnvFile} file is loaded`)
     }
     defaultLogger.debug('envs: ', process.env)
 }
