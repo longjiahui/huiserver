3c95d2cc43d4e7bd0541d8916d3fa0a1b0476572
Anfo <longjiahui@hotmail.com>
2024-12-18 15:13:05 +0800

    fix: fix type

--- src/util/jwt.ts
+++ src/util/jwt.ts
@@ -10,14 +10,9 @@ if (!jwtCookieSecret) {
 export const getAuthFunction = (secret: string) => ({
     sign: (
         data: string | Buffer | object,
-        options?: Parameters<typeof jwtSign>[2]
+        options?: Partial<Parameters<typeof jwtSign>[2]>
     ) =>
         jwtSign(data, secret, {
-            ...(data && typeof data === 'object'
-                ? {
-                      expiresIn: '30d',
-                  }
-                : {}),
             ...options,
         }),
     decode: (token: string) => jwtDecode(token),
